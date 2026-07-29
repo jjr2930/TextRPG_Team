82bdaa0f579d3d54b4b99a343fff24756c3fac89
ROCKMANZEROS <dudwlsdl34@gmail.com>
2026-07-29 17:25:23 +0900

    보스 틀 생성
    틀만 생성함

---
+++ Boss.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Boss.h"
+#include "Character.h"
+
+Boss::Boss(Character* Character)
+{
+	name = "오지게 강하고 고져스한 판타스틱 뷰티풀 지져스 간지나게 잘생긴 마왕";
+
+	//int firstHP = (int)(Character->GetMaxHP() * 1.5f);
+	//int randomHP = (rand() % (firstHP / 5)) - (firstHP / 10);
+	//hp = firstHP + randomHP;
+
+	//int firstPower = (int)(Character->GetAttack() * 1.5f);
+	//int random = (rand() % (firstPower / 5 > 0) ? ())
+}
