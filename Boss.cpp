e9b5c6bc686a13f9846fd0b750bd072ed9100634
ROCKMANZEROS <dudwlsdl34@gmail.com>
2026-07-29 20:51:21 +0900

    보스 공격 및 엔딩 추가
    이게 맞는건가? 일단 엔딩 출력 및 크래딧ㅠ

--- Boss.cpp
+++ Boss.cpp
@@ -16,7 +16,45 @@ void Boss::Attack(Character* Character)
 	if (damage < 0)
 		damage = 1;
 
+	int newCharacterHP = Character->GetCurrentHP() - damage;
+	if (newCharacterHP <= 0)
+		newCharacterHP = 0;
+
+	Character->SetCurrentHP(newCharacterHP);
+
 
 	std::cout << "마왕에 성스러운 마검을 휘둘렀다.\n";
-	std::cout << damage << " 데미지 공격!\n";
+	std::cout << "플레이어에게 " << damage << " 데미지 공격!\n";
+	std::cout << "플레이어의 현재 HP: " << Character->GetCurrentHP() << "\n";
+	std::cin.get();
+
+	if (Character->GetCurrentHP() <= 0) {
+		std::cout << "===================================================\n";
+		std::cout << "                  [ GAME OVER ]\n";
+		std::cout << " 마왕에 공격에 플레이어가 쓰러졌습니다.\n";
+		std::cout << "===================================================\n";
+
+		std::cin.get();
+		exit(0);
+	}
+
+	if (hp <= 0) {
+		std::cout << "===================================================\n";
+		std::cout << "                  [ GAME CLEAR ]\n";
+		std::cout << " 마왕에 사라지고 어둠이 사라졌습니다.\n";
+		std::cout << " 플레이어의 이름은 영원히 기억될 것 입니다.\n";
+		std::cout << "===================================================\n";
+		std::cout << "            - RPG GAME CLEAR & CREDITS -\n";
+		std::cout << "                   팀장 - 주정열\n";
+		std::cout << "                  매니저 - 천우진\n";
+		std::cout << "                   팀원 - 김규정\n";
+		std::cout << "                   팀원 - 황승용\n";
+		std::cout << "                   팀원 - 조수현\n";
+		std::cout << "                   팀원 - 장영민\n";
+		std::cout << " ==================================================\n";
+
+		std::cin.get();
+		exit(0);
+	}
+
 }
