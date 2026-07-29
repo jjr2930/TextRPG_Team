4c515f2e4d4ff486178cedcfed6f48a0cfc2c8c0
kimgj9311 <kimgj9311@gmail.com>
2026-07-29 19:26:48 +0900

    inventory

--- Inventory.h
+++ Inventory.h
@@ -9,13 +9,7 @@ class Inventory
 public:
 
     std::vector<Inventory>backpack;
-    std::vector<Inventory>potionPouch;
 
-    std::string name;
-    int itemID;
-    int QTY;
-    int maxQTY = 64;
-    
-    void addItem(int itemID, int addQTY);
+    void addItem(int itemID, int addQuantity);
 
 };
