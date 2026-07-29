eed1d74870d00835db7d1c6522514a5e63a03a1e
jjr2930 <joojungyeol@gmail.com>
2026-07-29 16:19:50 +0900

    [Mod] 콘솔이 한글을 잘 사용하도록 utf8 적용한다.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <Windows.h>
+
 #include "GameManager.h"
 
 int main(int argc, char *argv[])
 {
+    // 콘솔 출력 인코딩을 UTF-8로 설정
+    SetConsoleOutputCP(CP_UTF8);
+
+    // 콘솔에서 한글 입력도 받을 경우 필요
+    SetConsoleCP(CP_UTF8);
+
+
     GameManager gameManager;
     gameManager.showMainMenu();
 
