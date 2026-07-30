dd191131f8ffe4f786795f64788c67f2cbffa9ae
jjr2930 <joojungyeol@gmail.com>
2026-07-30 11:20:00 +0900

    [Mod] MainMenu 무한루프 추가, 중괄호

--- Shop.cpp
+++ Shop.cpp
@@ -8,36 +8,39 @@ Shop::Shop()
 {
 }
 
-void Shop::PrintMainMenu()
-{
-    cout << "나의 상점에 오신것을 환영합니다." << endl;;
-    cout << "1. 판매" << endl;
-    cout << "2. 구매" << endl;
-    cout << "3. 종료" << endl;
+void Shop::PrintMainMenu() {
+    while (true) {
+        cout << "상점에 오신것을 환영합니다." << endl;;
+        cout << "1. 판매" << endl;
+        cout << "2. 구매" << endl;
+        cout << "3. 종료" << endl;
 
-    SelectedMenuType choice;
-    int input;
-    cin >> input;
-    choice = static_cast<SelectedMenuType>(input);
-    switch (choice)
-    {
-        case SelectedMenuType::Sell:
-            PrintSellMenu();
-            break;
-        case SelectedMenuType::Buy:
-            PrintBuyMenu();
-            break;
-        case SelectedMenuType::Exit:
-            break;
-        default:
-            break;
+        SelectedMenuType choice;
+        int input;
+        cin >> input;
+        choice = static_cast<SelectedMenuType>(input);
+        switch (choice) {
+            case SelectedMenuType::Sell:
+                PrintSellMenu();
+                break;
+            
+            case SelectedMenuType::Buy:
+                PrintBuyMenu();
+                break;
+
+            case SelectedMenuType::Exit:
+                return;
+
+            default:
+                cout << "잘못된 입력입니다. 다시 시도해주세요." << endl;
+                break;
+        }
     }
 }
 
 void Shop::PrintSellMenu()
 {
-    while (true)
-    {
+    while (true) {
         cout << "판매할 아이템을 선택하세요." << endl;
         cout << "1 ~ 9: 판매할 아이템" << endl;
         cout << "0: 종료" << endl;
@@ -46,27 +49,22 @@ void Shop::PrintSellMenu()
         int input;
         cin >> input;
 
-        if (1 <= input && input <= 9)
-        {
+        if (MIN_ITEM_NUMBER_FOR_SALE <= input && input <= MAX_ITEM_NUMBER_FOR_SALE) {
             //checking out of range;
             //1. remove item from player inventory
             //2. add gold to player inventory
         }
-        else if (input == 0)
-        {
+        else if (input == 0) {
             break;
         }
-        else
-        {
+        else {
             cout << "Invalid input, please try again." << endl;
         }
     }
 }
 
-void Shop::PrintBuyMenu()
-{
-    while (true)
-    {
+void Shop::PrintBuyMenu() {
+    while (true) {
         cout << "구매할 아이템을 선택하세요." << endl;
         cout << "1 ~ 9: 구매할 아이템" << endl;
         cout << "0: 종료" << endl;
@@ -75,18 +73,15 @@ void Shop::PrintBuyMenu()
         int input;
         cin >> input;
 
-        if (1 <= input && input <= 9)
-        {
+        if (MIN_ITEM_NUMBER_FOR_BUY <= input && input <= MAX_ITEM_NUMBER_FOR_BUY) {
             //checking out of range;
             //1. remove gold from player inventory
             //2. add item to player inventory
         }
-        else if (input == 0)
-        {
+        else if (input == 0) {
             break;
         }
-        else
-        {
+        else {
             cout << "잘못된 입력입니다. 다시 시도해주세요." << endl;
         }
     }
--- Shop.h
+++ Shop.h
@@ -17,6 +17,11 @@ enum class SelectedMenuType
 class Shop
 {
 public:
+    static constexpr int MIN_ITEM_NUMBER_FOR_SALE = 1;
+    static constexpr int MAX_ITEM_NUMBER_FOR_SALE = 9;
+    static constexpr int MIN_ITEM_NUMBER_FOR_BUY = 1;
+    static constexpr int MAX_ITEM_NUMBER_FOR_BUY = 9;
+
     Shop();
 public:
     void PrintMainMenu();
