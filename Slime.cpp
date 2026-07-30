a0184784f9110393ee48fe8123df44aeb98adb6a
seancho6616 <whtngus6616@gmail.com>
2026-07-30 20:24:22 +0900

    [Mod] 조건문 if 코딩 컨벤션 맞춤

--- Slime.cpp
+++ Slime.cpp
@@ -10,10 +10,10 @@ void Slime::Attack(Battle* other) {
 	other->TakeDamage(power);
 	std::cout << name << "이(가) 공격하였습니다. (" << power << "\n";
 }
+
 void Slime::TakeDamage(int damage) {
 	hp -= damage;
 	std::cout << damage << "만큼 " << name << "이(가) 당했습니다.\n";
-	if (hp <= 0) {
+	if (hp <= 0) 
 		std::cout << name << "죽었습니다.\n";
-	}
 }
