9fce6be59ec425675eebc270b25b0e9953f812b0
seancho6616 <whtngus6616@gmail.com>
2026-07-31 12:07:50 +0900

    [Mod]배틀클래스 변경으로 공격 메서드 수정

--- Slime.cpp
+++ Slime.cpp
@@ -6,8 +6,8 @@
 
 Slime::Slime(Item* item, int level) : Monster("Slime", 20, 30, 5, 10, 30, item, 20, level) {};
 
-void Slime::Attack(Battle* other) {
-	other->TakeDamage(power);
+void Slime::Attack() {
+	target->TakeDamage(power);
 	std::cout << name << "이(가) 공격하였습니다. (" << power << "\n";
 }
 
--- Slime.h
+++ Slime.h
@@ -5,7 +5,7 @@ class Slime : public Monster{
 public:
 	Slime(Item* item, int level);
 
-	virtual void Attack(Battle* other) override;
+	virtual void Attack() override;
 	virtual void TakeDamage(int damage) override;
 };
 
