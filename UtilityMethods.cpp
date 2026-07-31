63b259d24b4e144962fe3a730d15bf1b0e6d6a6b
jjr2930 <joojungyeol@gmail.com>
2026-07-31 20:51:24 +0900

    [Del] 중복되는 기능 삭제

--- UtilityMethods.cpp
+++
@@ -1,9 +0,0 @@
-#include "UtilityMethods.h"
-#include <iostream>
-#include <conio.h>
-
-void UtilityMethods::WaitForKey()
-{
-    std::cout << std::endl << "계속하려면 아무 키나 누르세요...\n";
-    _getch();
-}
