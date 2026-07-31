a5292997db654cb112c403901a43843b3e37b051
seancho6616 <whtngus6616@gmail.com>
2026-07-31 12:06:45 +0900

    [Mod] 난수를 생성하는 코드 멤버변수로 수정

--- Random.cpp
+++ Random.cpp
@@ -1,9 +1,12 @@
-#include <random>
 #include "Random.h"
 
+Random::Random() : gen(std::random_device{}()) {}
+
 int Random::GetRandomValue(int min, int max) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(min, max);
     return dis(gen);
 }
+/*std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(min, max);
+    return dis(gen);*/
--- Random.h
+++ Random.h
@@ -1,6 +1,12 @@
 #pragma once
+#include <random>
+
+
 class Random {
+private:
+	std::mt19937 gen;
 public:
+	Random();
 	int GetRandomValue(int min, int max);
 };
 
