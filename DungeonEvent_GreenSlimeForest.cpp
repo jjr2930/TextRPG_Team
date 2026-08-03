e3356dd82c0e42b5cca89749f0f25cf5e4016e0b
cdw0711 <woojin0711@gmail.com>
2026-08-03 10:31:37 +0900

    Create DungeonEvent_GreenSlimeForest.cpp

---
+++ DungeonEvent_GreenSlimeForest.cpp
@@ -0,0 +1,11 @@
+#include "DungeonEvent_GreenSlimeForest.h"
+
+GreenSlimeForestEvent::GreenSlimeForestEvent(Character& character) : DungeonEvent(character) {}
+
+void GreenSlimeForestEvent::RandomEvent() {
+    // Implement random event logic for Green Slime Forest
+}
+
+void GreenSlimeForestEvent::BossEvent() {
+    // Implement boss event logic for Green Slime Forest
+}
